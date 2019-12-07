943ddd0f3d626567730c9214b75e09f22c54589b
chuckwilliams37 <chuck@dash.org>
2019-12-07 13:56:49 -0500

    Adds

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,12 +17,39 @@ const IndexPage = () => (
 
   <Helmet>
   <script async src="https://www.googletagmanager.com/gtag/js?id=UA-154121766-1"></script>
-  <script type="application/ld+json">{`
+  <script type="application/javascript">{`
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('config', 'UA-154121766-1');
       gtag('event', 'pageview', 'Home');
-  `}</script>
+      `}</script>
+
+
+      <script type="application/javascript">{`
+    "use strict";
+    !function() {
+      var t = window.driftt = window.drift = window.driftt || [];
+      if (!t.init) {
+        if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
+        t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ], 
+        t.factory = function(e) {
+          return function() {
+            var n = Array.prototype.slice.call(arguments);
+            return n.unshift(e), t.push(n), t;
+          };
+        }, t.methods.forEach(function(e) {
+          t[e] = t.factory(e);
+        }), t.load = function(t) {
+          var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
+          o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
+          var i = document.getElementsByTagName("script")[0];
+          i.parentNode.insertBefore(o, i);
+        };
+      }
+    }();
+    drift.SNIPPET_VERSION = '0.3.1';
+    drift.load('azinkdvzwirg');
+    `}</script>
   </Helmet>
 
     <Sidebar />
